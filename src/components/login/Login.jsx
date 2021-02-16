4adf6c0176ba1f5ad4fb282aa98a9e0ad50619f9
Luiz Fernando <liko.12@hotmail.com>
2021-02-16 20:31:16 -0300

    Login e Cadastro On

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,82 +1,72 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
+import {Login,Cadastrar} from './js/User'
 import "./login.css"
-import axios from "axios"
 
 
 
 
 
 
-export default function Login(e) {
-    const [user, setUser] = useState("")
-    const [pass, setPass] = useState("")
-    const Post = {}
-
-    Post.username = user
-    Post.password = pass
-
-
-
-    const Cadastrar = (event) => {
-
-        event.preventDefault()
-
-        axios.post("https://keener-back.herokuapp.com/user/create", Post)
-            .then(resp => {
-                const res = resp.data
-                console.log(res)
-                alert("Cadastro Realizado")
-            })
-            .catch(e => {
-                console.log(e)
-                alert("Não foi possível realizar o cadastro")
-            })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }
-
-    const Login = (event) => {
-
-        event.preventDefault()
-
-        axios.post("https://keener-back.herokuapp.com/user/login", Post)
-            .then(resp => {
-                const res = resp.data
-                console.log(res)
-                
-            })
-            .catch(e => {
-                console.log(e)
-                alert("Username ou Password invalid")
-            })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }
-
-    
 
 
 
 
 
+   
+    const LoginScreen = ({ history }) => {
 
-return (
+        const [user, setUser] = useState("");
+        const [password, setPassword] = useState("");
 
-    <div className="container">
-        <label id="lbl1">
-            Username
+        const doLogin = async () => {
+            try {
+                if (!user /* validação de user */) {
+                    return alert("User inválido");
+                }
+                if (!password /* validação de password */) {
+                    return alert("Senha inválida");
+                }
+                const userData = await Login(user, password);
+
+                localStorage.setItem("userData", JSON.stringify(userData));
+
+                history.push("/app");
+            } catch (e) {
+                alert("User or password invalid " + e.message);
+            }
+        };
+
+        const signIn = async () =>{
+
+            await Cadastrar(user, password);
+
+
+
+        }
+
+
+        return (
+
+            <div className="container">
+                <label id="lbl1">
+                    Username
             <input type="text" name="username" onChange={(e) => { setUser(e.target.value) }} />
-        </label>
-        <label id="lbl2">
-            Password
-            <input type="password" name="password" onChange={(e) => { setPass(e.target.value) }} />
-        </label>
-        <input id="btn1" type="submit" value="Enviar" onClick={Login}/>
-        <input id="btn2" type="submit" value="Cadastrar" onClick={Cadastrar} />
-    </div>
+                </label>
+                <label id="lbl2">
+                    Password
+            <input type="password" name="password" onChange={(e) => { setPassword(e.target.value) }} />
+                </label>
+                <input id="btn1" type="submit" value="Entrar" onClick={doLogin} />
+                <input id="btn2" type="submit" value="Cadastrar" onClick={signIn} />
+            </div>
 
-)
-
-
+        )
 
 
 
-}
+
+
+    };
+
+
+    export default LoginScreen;
